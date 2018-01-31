2d517338e50effad5e9f9d2354dc7672ad1092b8
pollockcl <pollock.clee@gmail.com>
2018-01-31 10:49:11 -0700

    added tests for reverse conversion

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -15,10 +15,26 @@ class TranslatorTest < MiniTest::Test
   end
 
   def test_convert_to_morse_method
-    assert_equal "-.-..--", @trans.convert_to_morse('cat')
-    assert_equal "...-----.", @trans.convert_to_morse('SOME')
-    assert_equal "-...----..-.--", @trans.convert_to_morse("B?!!..O,..@d##y")
-    assert_equal "....--..--.-..... .-----.-.-.-", @trans.convert_to_morse("spaces work")
-    assert_equal ".----.----..---..------.......-----", @trans.convert_to_morse("1122850")
+    assert_equal "-.-. .- -", @trans.convert_to_morse('cat')
+    assert_equal "... --- -- .", @trans.convert_to_morse('SOME')
+    assert_equal "-... --- -.. -.--", @trans.convert_to_morse("B?!!..O,..@d##y")
+    assert_equal "... .--. .- -.-. . ... / .-- --- .-. -.-", @trans.convert_to_morse("spaces work")
+    assert_equal ".---- .---- ..--- ..--- ---.. ..... -----", @trans.convert_to_morse("1122850")
   end
+
+  def test_convert_file_to_morse
+    assert_equal "-.. .. -.. / -.-- --- ..- / . ...- . .-. / .... . .- .-. / - .... . / - .-. .- --. . -.. -.-- / --- ..-. / -.. .- .-. - .... / .--. .-.. .- --. ..- . .. ... / - .... . / .-- .. ... .", 
+    @trans.convert_file_to_morse('./tragedy.txt')
+  end
+
+  def test_convert_from_morse_method
+    assert_equal 'cat', @trans.convert_from_morse('-.-. .- -')
+    assert_equal 'younglings', @trans.convert_from_morse('-.-- --- ..- -. --. .-.. .. -. --. ...')
+    assert_equal "8936740221", @trans.convert_from_morse('---.. ----. ...-- -.... --... ....- ----- ..--- ..--- .----')
+  end
+
+  def test_convert_file_from_morse
+    assert_equal 'some body once told me the world is gonna roll me i aint the sharpest tool in the shed', @trans.convert_file_from_morse('./morse.txt')
+  end
+
 end
